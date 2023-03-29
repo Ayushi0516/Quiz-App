cf7c994796cf395b847f412b3157df6c0e493aae
Ayushi0516 <ayushigupta0516@gmail.com>
2023-03-29 14:34:44 +0530

    routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 
 import './App.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import { Quiz } from './component/Quiz';
+import { Result } from './component/Result';
+import { Main } from './component/Main';
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Main></Main>
+  },
+  {
+    path:"/quiz",
+    element:<Quiz></Quiz>
+  },
+  {
+    path:"/result",
+    element:<Result></Result>
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <p>quiz app</p>
-    </div>
+    <>
+    <RouterProvider router={router}/>
+    </>
+    
   );
 }
 
